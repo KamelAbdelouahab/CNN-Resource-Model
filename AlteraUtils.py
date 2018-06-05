d71063aa315b4d34320f90f9be6add4f28d1e1b7
Kamel Abdelouahab <kamel_eddine.abdelouahab@uca.fr>
2018-06-05 16:00:52 +0100

    Overloaded getALM to provide ALMs for specific instance name

--- AlteraUtils.py
+++ AlteraUtils.py
@@ -20,3 +20,41 @@ def writeLine(file_name, line_number, line):
     lines[line_number] = line
     with open(file_name, 'w') as g:
         g.writelines(lines)
+
+def getALM(fit_rpt_filename,instance_name):
+    num_alm_pos = 6 # Number of ALMs used is the 6th word of the line
+    instance_pos = [] # name of instance in the 1st word of line (after ;)
+    instance = []
+    num_alm = []
+    fp = open(fit_rpt_filename)
+    instance_occur = 0; #have to add instance index to have proper out dictionary
+    #
+
+    for i,line in enumerate(fp):
+        if instance_name in line:
+            instance_occur += 1
+            num_alm.append(line.split()[6])
+    num_alm = list(map(float,num_alm))
+    instance_number = listAsQuartus(instance_occur)
+    d = (dict(zip(instance_number,num_alm)))
+    return d
+
+def dispSortedDict(dictionary):
+    for key in sorted(dictionary):
+        print("%s: %s" % (key, dictionary[key]))
+
+def listAsQuartus(length):
+    l = []
+    for x in range(length):
+        l.append(x)
+    l = sorted(l,key=str)
+    l.insert(11, l.pop(1))
+    return l
+
+if __name__ == '__main__':
+    fit_rpt = "example/quartus/FittingReport.txt"
+    # instance_name = ";          |MOA:MOA_i|"
+    instance_name = ";          |MCM:MCM_i|"
+    # instance_name = ";       |DotProduct:"
+    dd = getALM(fit_rpt, instance_name)
+    dispSortedDict(dd)
--- Models.py
+++ Models.py
@@ -1,13 +1,12 @@
 import numpy as np
 import os
 import caffe
+import AlteraUtils
 
 def quantizeWeight(float_weight,bitwidth):
     scale_factor = 2**(bitwidth-1) - 1
     scaled_data = np.round(float_weight * scale_factor)
     return np.array(scaled_data, dtype=int)
-
-
 
 def costMOA(n_opd):
     # coef = 3.23636364
@@ -38,8 +37,18 @@ def costTensorExtractor(image_width, kernel_size):
 def costConv(kernel_list,bitwidth):
     j_conv = np.zeros([kernel_list.shape[0],2],dtype=float)
     for n in range(kernel_list.shape[0]):
-        j_conv[n,:] = costDotProduct(kernel_list[n].flatten())
-    print(j_conv)
+        j_conv[n,0:2] = costDotProduct(kernel_list[n].flatten())
+    return(j_conv)
+
+def trueConv(fit_rpt_filename):
+    instance_name = ";          |MOA:MOA_i|"
+    true_moa = AlteraUtils.getALM(fit_rpt_filename, instance_name)
+    instance_name = ";          |MCM:MCM_i|"
+    true_scm = AlteraUtils.getALM(fit_rpt_filename, instance_name)
+    true_conv = np.zeros([len(true_moa),2],dtype=float)
+    true_conv[:,0] = np.fromiter(true_scm.values(), dtype=float)
+    true_conv[:,1] = np.fromiter(true_moa.values(), dtype=float)
+    return(true_conv)
 
 if __name__ == '__main__':
     project_root = ""
@@ -50,4 +59,12 @@ if __name__ == '__main__':
     net = caffe.Net(proto_file,model_file,caffe.TEST)
     conv1 = net.params['conv1'][0].data
     conv1 = quantizeWeight(conv1,bitwidth)
-    costConv(conv1,bitwidth)
+    model = costConv(conv1,bitwidth)
+
+    fit_rpt_filename = "example/quartus/FittingReport.txt"
+    true_val = trueConv(fit_rpt_filename)
+    # print(model)
+    # print(true_val)
+    print(100*model[:,1]/true_val[:,1])
+    # AlteraUtils.dispSortedDict(true_moa)
+    # AlteraUtils.dispSortedDict(true_scm)
