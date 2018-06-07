44ab183f23505479d8757a82c92a30c81fd152ef
Kamel Abdelouahab <kamel_eddine.abdelouahab@uca.fr>
2018-06-07 15:03:15 +0100

    Updated AlteraUtils to get ALMs from fit report

--- AlteraUtils.py
+++ AlteraUtils.py
@@ -1,9 +1,9 @@
 import sys
 import os
 
-def getALM(fit_filename):
+def getSummaryALM(fit_filename):
     fp = open(fit_filename)
-    lineALM,wordALM = (7,5)      # ALM utilization is in line 8, word 6 of .fit.summary file
+    lineALM,wordALM = (7,5)             # ALM utilization is in line 8, word 6
     for i,line in enumerate(fp):
         if (i == lineALM):
             print(line.split()[wordALM])
@@ -22,18 +22,39 @@ def writeLine(file_name, line_number, line):
         g.writelines(lines)
 
 def getALM(fit_rpt_filename,instance_name):
-    num_alm_pos = 6 # Number of ALMs used is the 6th word of the line
-    instance_pos = [] # name of instance in the 1st word of line (after ;)
+    num_alm_pos = 6          # Number of ALMs used is the 6th word of the line
+    instance_pos = [] # name of instance in the 1st word of line (after the ;)
     instance = []
     num_alm = []
     fp = open(fit_rpt_filename)
-    instance_occur = 0; #have to add instance index to have proper out dictionary
-    #
+    instance_occur = 0;
+    # have to add instance index to have proper out dictionary
 
     for i,line in enumerate(fp):
         if instance_name in line:
             instance_occur += 1
-            num_alm.append(line.split()[6])
+            # ALMs Needed = 3 , ALMs Used = 6
+            num_alm.append(line.split()[3])
+    num_alm = list(map(float,num_alm))
+    instance_number = listAsQuartus(instance_occur)
+    d = (dict(zip(instance_number,num_alm)))
+    return d
+
+def getALMStop(fit_rpt_filename,instance_name, stop_inst_name):
+    num_alm_pos = 6          # Number of ALMs used is the 6th word of the line
+    instance_pos = [] # name of instance in the 1st word of line (after the ;)
+    instance = []
+    num_alm = []
+    fp = open(fit_rpt_filename)
+    instance_occur = 0;
+    # have to add instance index to have proper out dictionary
+
+    for i,line in enumerate(fp):
+        if instance_name in line:
+            instance_occur += 1
+            num_alm.append(line.split()[3])
+        if stop_inst_name in line:
+            break
     num_alm = list(map(float,num_alm))
     instance_number = listAsQuartus(instance_occur)
     d = (dict(zip(instance_number,num_alm)))
@@ -44,17 +65,21 @@ def dispSortedDict(dictionary):
         print("%s: %s" % (key, dictionary[key]))
 
 def listAsQuartus(length):
+    #    0 1 10 11 12 13 14 15 16 17 18 19 2 20 21 22 23 24 25 26 27 28 29
+    # -> 0 10 11 12 13 14 15 16 17 18 19 1 2 20 21 22 23 24 25 26 27 28 29
+    # -> 0 10 11 12 13 14 15 16 17 18 19 1 20 21 22 23 24 25 26 27 28 29 2
     l = []
     for x in range(length):
         l.append(x)
     l = sorted(l,key=str)
-    l.insert(11, l.pop(1))
+    x,y = 0,0;
+    while y < length: #BUG HERE: But im too tired
+        x = y + 1
+        y = 11 + y
+        if y > length:
+            break
+        else:
+            l.insert(y, l.pop(x))
+    # print("Quartus List")
+    # print(l)
     return l
-
-if __name__ == '__main__':
-    fit_rpt = "example/quartus/FittingReport.txt"
-    # instance_name = ";          |MOA:MOA_i|"
-    instance_name = ";          |MCM:MCM_i|"
-    # instance_name = ";       |DotProduct:"
-    dd = getALM(fit_rpt, instance_name)
-    dispSortedDict(dd)
